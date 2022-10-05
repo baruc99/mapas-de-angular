395b62ca780d0cecfedbd7d5cfbea2439d5ff481
[baruc] <[barucgs@gmail.com]>
2022-10-05 18:45:56 -0500

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoiYmFydWNncyIsImEiOiJjbDh1aWVxejQwNGI3M3dsbXp3ZXU3dHFoIn0.ZNQbq3ztImkol_BkzrwUUg'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoiYmFydWNncyIsImEiOiJjbDh1aWVxejQwNGI3M3dsbXp3ZXU3dHFoIn0.ZNQbq3ztImkol_BkzrwUUg'
 };
 
 /*
